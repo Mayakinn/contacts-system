9af9343bde798eff415e8784206bc2a0f0cedadb
Maja V <maja.vinceviciute@teltonika.lt>
2025-07-07 15:54:35 +0300

    Need to finish styling

---
+++ src/components/pageComponents/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="flex bg-blue-900 max-h-25">
+    <img class="h-auto w-auto object-contain object-left" src="../../../public/Logo.png" />
+    <div class="flex">
+      <RouterLink to="/" class="font-bold rounded-lg px-3 py-2 text-lg text-white m-7"
+        >Kontaktai</RouterLink
+      >
+      <RouterLink to="/companies" class="font-bold text-white text-lg rounded-lg px-3 py-2 m-7"
+        >Įmonės</RouterLink
+      >
+      <RouterLink to="/structure" class="font-bold text-lg text-white rounded-lg px-3 py-2 m-7"
+        >Struktūra</RouterLink
+      >
+      <RouterLink to="/admin" class="font-bold text-lg rounded-lg text-white px-3 py-2 m-7"
+        >Paskyros</RouterLink
+      >
+      <RouterLink to="/login" class="font-bold text-lg text-lgrounded-lg text-white px-3 py-2 m-7"
+        >Prisijungti</RouterLink
+      >
+    </div>
+  </nav>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,9 @@
+import AdminView from '@/views/AdminView.vue'
+import CompaniesManagementView from '@/views/CompaniesManagementView.vue'
+import CompanyStructureView from '@/views/CompanyStructureView.vue'
 import ContactView from '@/views/ContactView.vue'
+import ForgotPasswordView from '@/views/ForgotPasswordView.vue'
+import LoginView from '@/views/LoginView.vue'
 import SingleContactView from '@/views/SingleContactView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -21,6 +26,31 @@ const router = createRouter({
           props: true,
         },
       ],
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/companies',
+      name: 'companies',
+      component: CompaniesManagementView,
+    },
+    {
+      path: '/structure',
+      name: 'structure',
+      component: CompanyStructureView,
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminView,
+    },
+    {
+      path: '/forgotpassword',
+      name: 'forgotpassword',
+      component: ForgotPasswordView,
     },
   ],
 })
