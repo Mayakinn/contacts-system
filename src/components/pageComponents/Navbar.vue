e236082810a0666d3929f8ce40df9dc60979da53
Maja V <maja.vinceviciute@teltonika.lt>
2025-07-09 08:17:34 +0300

    Resolve "Router"

---
+++ src/components/pageComponents/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="flex items-center justify-between bg-teltonika-blue px-20 py-3">
+    <img class="h-auto w-100 object-contain" src="../../../Logo.png" alt="Logo" />
+    <div class="flex space-x-40 text-2xl text-white font-semibold">
+      <RouterLink to="/" class="hover:text-gray-300 transition-colors">Kontaktai</RouterLink>
+      <RouterLink to="/companies" class="hover:text-gray-300 transition-colors">Įmonės</RouterLink>
+      <RouterLink to="/structure" class="hover:text-gray-300 transition-colors"
+        >Struktūra</RouterLink
+      >
+      <RouterLink to="/admin" class="hover:text-gray-300 transition-colors">Paskyros</RouterLink>
+      <RouterLink to="/login" class="hover:text-gray-300 transition-colors">Prisijungti</RouterLink>
+    </div>
+  </nav>
+</template>
