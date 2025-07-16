e2ff541672f822781da987324d91406fb75454c0
Maja V <maja.vinceviciute@teltonika.lt>
2025-07-16 09:15:14 +0300

    almost finished styling

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import Login from '@/components/loginComponents/Login.vue'
 import router from '@/router'
 </script>
 
-<template>
+<template class="">
   <div class="relative" @click="router.back()">
-    <div
-      class="shadow-lg hover:bg-gray-200 cursor-pointer rounded-full w-10 h-10 items-center flex absolute top-10 left-40"
+    <button
+      class="shadow-lg hover:bg-gray-400 bg-white cursor-pointer rounded-full w-10 h-10 items-center flex absolute top-20 left-40"
     >
       <img class="size-5 m-2.5 mt-3" src="../assets/curved-arrow.png" />
+    </button>
+  </div>
+  <div class="min-h-screen bg-teltonika-blue flex items-center justify-center">
+    <div class="bg-white">
+      <Login />
     </div>
   </div>
 </template>
