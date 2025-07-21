e69ed31bdaa6c1952fd5cadcf0cef87751c28d91
Maja V <maja.vinceviciute@teltonika.lt>
2025-07-21 09:43:38 +0300

    start of admin

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup>
+import AdminTableList from '@/components/adminComponents/AdminTableList.vue'
 import FormModal from '@/components/modalComponents/FormModal.vue'
+import type { User } from '@/typings/interface/User'
 import { shallowRef, ref, type Component } from 'vue'
 const adminId = ref<number>(0)
 
 const currentForm = shallowRef<Component>()
 const formModalActive = ref(false)
+const users = ref<User[]>()
 
 const closeModal = () => {
   formModalActive.value = false
@@ -22,7 +25,6 @@ function OpenModal() {
 </script>
 
 <template>
-  <button @click="OpenModal">Press for modal</button>
   <FormModal :isActive="formModalActive" @close-modal="closeModal">
     <component
       @close-pressed="closeModalAfterForm"
@@ -30,5 +32,21 @@ function OpenModal() {
       :adminId="adminId"
     ></component>
   </FormModal>
-  <div>Admin management page</div>
+  <div class="m-10 space-y-10">
+    <p class="text-5xl font-light">Admin paskyros</p>
+    <div class="flex space-x-10">
+      <button
+        class="w-10 h-10 bg-button-blue rounded-full flex items-center justify-center shadow-2xl hover:bg-blue-800"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="30" height="30">
+          <path
+            fill="white"
+            d="M10.75 5.75c0-.414-.336-.75-.75-.75s-.75.336-.75.75v3.5h-3.5c-.414 0-.75.336-.75.75s.336.75.75.75h3.5v3.5c0 .414.336.75.75.75s.75-.336.75-.75v-3.5h3.5c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-3.5v-3.5Z"
+          />
+        </svg>
+      </button>
+      <p class="flex items-center justify-center">Sukurti naują admin paskyrą</p>
+    </div>
+    <AdminTableList :users="users" />
+  </div>
 </template>
