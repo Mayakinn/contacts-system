a2507fbf57afb5e353d35ebf9b627ea44e0af7c8
Maja V <maja.vinceviciute@teltonika.lt>
2025-07-21 09:43:38 +0300

    start of admin

---
+++ src/components/adminComponents/AdminTableList.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { Contact } from '@/typings/interface/Contact'
+import AdminTableRow from './AdminTableRow.vue'
+import { useRoute, useRouter } from 'vue-router'
+import type { User } from '@/typings/interface/User'
+
+const props = defineProps<{
+  users: User[] | undefined
+}>()
+const router = useRouter()
+</script>
+
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg mr-30">
+    <table class="text-sm text-left w-full">
+      <thead class="text-xl text-center bg-gray-50">
+        <tr>
+          <th scope="col" class="px-6 py-3">Vardas</th>
+          <th scope="col" class="px-6 py-3">El. paštas</th>
+          <th scope="col" class="px-6 py-3">Veiksmas</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="user in props.users"
+          class="bg-white border-b border-gray-200 hover:bg-gray-200 text-center"
+        >
+          <AdminTableRow :user="user" />
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
---
+++ src/components/adminComponents/AdminTableRow.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { Contact } from '@/typings/interface/Contact'
+import type { User } from '@/typings/interface/User'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  user: User | undefined
+}>()
+</script>
+
+<template>
+  <th class="px-6 py-4 font-medium whitespace-nowrap">
+    {{ props.user?.name }}
+  </th>
+  <td class="px-6 py-4">{{ props.user?.email }}</td>
+  <td class="px-6 py-4"></td>
+</template>
