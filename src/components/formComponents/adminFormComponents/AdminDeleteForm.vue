f5fc1dae8637e701fa982ad3e9974d138acf7903
majvin <maja.vinceviciute@teltonika.lt>
2025-07-26 20:19:00 +0300

    bug fixes

--- src/components/formComponents/adminFormComponents/AdminDeleteForm.vue
+++ src/components/formComponents/adminFormComponents/AdminDeleteForm.vue
@@ -6,24 +6,24 @@ import { NotificationType } from '@/typings/interface/NotificationType';
 import type { User } from '@/typings/interface/User';
 const emit = defineEmits(['close-pressed'])
 const props = defineProps<{
-  currentAdmin: User | null
+    currentAdmin: User | null
 }>()
 const notif = useNotificationStore()
 
-async function deleteSelectedAdmin(){
-    if (props.currentAdmin?.username == 'admin' ){
+async function deleteSelectedAdmin() {
+    if (props.currentAdmin?.username == 'admin') {
         notif.addNotification("Negalima panaikinti šios paskyros!", NotificationType.danger)
         emit('close-pressed', true)
         return
     }
 
-    try{
-        if(props.currentAdmin != null){
+    try {
+        if (props.currentAdmin != null) {
             await deleteAdmin(props.currentAdmin)
             notif.addNotification('Sėkmingai panaikinta paskyra', NotificationType.success)
             emit('close-pressed')
         }
-    } catch (error : any){
+    } catch (error: any) {
         notif.addNotification(error.message, NotificationType.danger)
         emit('close-pressed', true)
     }
@@ -36,11 +36,9 @@ async function deleteSelectedAdmin(){
         <h1 class="text-xl">Ar tikrai norite ištrinti paskyrą?</h1>
         <h1 class="text-sm text-gray-700">Paskyros vardas: {{ currentAdmin?.name }}</h1>
         <div class="absolute text-blue-500 bottom-0 right-0 flex">
-            <ModalCloseButton :isDeleteModal="true" @close-modal="emit('close-pressed')"/>
-            <button
-            class="hover:bg-gray-300 rounded-xl p-2" @click="deleteSelectedAdmin"
-            >
-            Taip
+            <ModalCloseButton :isDeleteModal="true" @close-modal="emit('close-pressed', true)" />
+            <button class="hover:bg-gray-300 rounded-xl p-2" @click="deleteSelectedAdmin">
+                Taip
             </button>
         </div>
     </div>
