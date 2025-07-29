e60cb4cb09f9624e2436fafebb73c026545f74c9
majvin <maja.vinceviciute@teltonika.lt>
2025-07-29 10:58:12 +0300

    text mistake

--- src/components/formComponents/companyFormComponents/CompanyDeleteForm.vue
+++ src/components/formComponents/companyFormComponents/CompanyDeleteForm.vue
@@ -27,7 +27,7 @@ async function deleteSelectedCompany() {
 <template>
   <div class="sm:items-start m-5 pr-50">
     <h1 class="text-xl">Ar tikrai norite ištrinti įmonę?</h1>
-    <h1 class="text-sm text-gray-700">Pavadinimas {{ currentCompany?.name }}</h1>
+    <h1 class="text-sm text-gray-700">Pavadinimas: {{ currentCompany?.name }}</h1>
     <div class="absolute text-blue-500 bottom-0 right-0 flex">
       <ModalCloseButton :isDeleteModal="true" @close-modal="emit('close-pressed', true)" />
       <button class="hover:bg-gray-300 rounded-xl p-2" @click="deleteSelectedCompany">Taip</button>
